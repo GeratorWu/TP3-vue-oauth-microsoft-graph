1a2194ba49c70a57da652d80bc130d6cfcc08b09
Patrick Wu <patrickwugerator@gmail.com>
2025-06-19 09:43:56 +0200

    Ex 4: create HomePage component

---
+++ src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<!-- src/components/HomePage.vue -->
+<template>
+  <div>Bienvenue sur la page d'accueil</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+
+<style>
+
+</style>
