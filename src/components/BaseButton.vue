b10ad042222b78dcb1d70fdf98087d3fd16e74c8
BoogieFox <mineclone14@gmail.com>
2025-06-19 11:35:05 +0200

    Ex 7: create BaseButton with primary color

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,66 @@
+<template>
+  <button
+    :class="['base-button', variantClass]"
+    :disabled="disabled"
+  >
+    {{ nom }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    variant: {
+      type: String,
+      default: 'primary', 
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    nom: {
+        type: String
+    }
+  },
+  computed: {
+    variantClass() {
+      return `base-button--${this.variant}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.base-button {
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 20px;
+}
+
+
+.base-button:hover{
+    background-color: white !important;
+    color: #04AA6D;
+    font-weight: bold;
+}
+
+.base-button:disabled {
+    filter: brightness(120%);
+}
+
+.base-button--primary {
+    background-color: #04AA6D !important; 
+    color: white;
+    border: 2px solid #04AA6D;
+}
+
+.base-button:disabled {
+  background-color: #cccccc;
+  cursor: not-allowed;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="homePage">
     <p>Bienvenue sur la page d'accueil</p>
+    <p>Voici les BaseButton :</p>
+    <BaseButton nom="BaseButton"/>
+    <BaseButton nom="BaseButton" disabled="true"/>
     </div>
 </template>
 
 <script>
+
+
+import BaseButton from './BaseButton.vue'
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    BaseButton
+  }
 }
 </script>
 
